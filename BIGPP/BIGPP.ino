e225c9d9b616e6fdb14133b23ce6e21d2b8613d8
Wunderharke9001 <arvedsteuer@gmail.com>
2020-05-13 19:34:37 +0200

    made pp a bit too big

--- BIGPP/BIGPP.ino
+++ BIGPP/BIGPP.ino
@@ -1,5 +1,6 @@
-#define Dleft x //mit Pins ersetzen
-#define Dright x
+#define Dleft 1 
+#define Dright 2
+#define hauptinput 3
 void setup() {
   // put your setup code here, to run once:
   /*
@@ -7,17 +8,48 @@ void setup() {
    A lternative
    Y oucontroll
    */
-int gay[2][5] = {{0,0,0,0,0}, {0,0,0,0,0}};
+  
+int gay[5][5];
 int Knopf = 0;
+/*
+  Y-axis
+  E thernal
+  E xtraction
+  T erminal
+  */
+int yeet = 0;
+/*
+B iological
+I nput
+G amma,
+P rime
+P oint
+*/
+bool bigpp = false;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   CUM();
   if(Knopf != 0){
-    
+    if(Knopf == 1){ if(yeet != 0){
+      yeet = yeet - 1;
+
+    }
+    }
+    if(Knopf == 2){ if(yeet != 5){
+      yeet = yeet + 1;
+
+    }
   }
 }
+
+if(bigpp==true){
+for()
+}
+
+  }
+
 /*
 C heck
 U serinput
@@ -25,7 +57,11 @@ M echanism
 */
  void CUM(){ //überprüft, welche knöpfe gedrückt wurden
    if(digitalRead(Dleft)==HIGH{ Knopf = 1;})
-   if(digitalRead(Dright)==HIGH{ Knopf = 2;})
+   if(digitalRead(Dright)==HIGH){ Knopf = 2;}
    else{Knopf=0;}
   return;
+ }
+ bool bigpp(){
+   if(digitalRead(hauptinput)==HIGH){return = true;}
+   return = false;
  }
